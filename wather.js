2c335f24a9c661f3045b5a05df82702f6156cbe0
SafeOwn <84808887+SafeOwn@users.noreply.github.com>
2025-07-22 13:01:04 +0300

    Update wather.js

--- wather.js
+++ wather.js
@@ -9,7 +9,7 @@
     this.create = function () {
       html = $(
         '<div class="weather-widget">' +
-          'Краснодар: <nobr>' 
+          '<div>Краснодар: </div><nobr>' 
           '<div class="weather-temp" id="weather-temp"> </div>' +
           '<div class="weather-locationName" id="weather-locationName"> </div>' +
           '<div class="weather-condition" id="weather-condition"></div>' +
