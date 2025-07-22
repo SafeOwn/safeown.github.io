05317d6eff2cc8a843377e095378bfbf925a1027
SafeOwn <84808887+SafeOwn@users.noreply.github.com>
2025-07-22 12:40:50 +0300

    Update wather.js

--- wather.js
+++ wather.js
@@ -40,6 +40,7 @@
       //onsole.log("Погода", "Обстановка: " + condition)
 
       $("#weather-temp").text(temp + "°");
+      $("locationName");
       $("#weather-condition")
         .text(condition)
         .toggleClass("long-text", condition.length > 10);
