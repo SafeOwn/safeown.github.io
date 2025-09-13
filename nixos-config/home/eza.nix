accbdeacfbfdab6489476f1007c4feec90870efa
SafeOwn <84808887+SafeOwn@users.noreply.github.com>
2025-09-13 18:37:02 +0300

    Delete nixos-config directory

--- nixos-config/home/eza.nix
+++
@@ -1,8 +0,0 @@
-{
-  programs.eza = {
-    enable = true;  # Устанавливает eza в систему
-    enableBashIntegration = true; # Добавляет алиасы в ~/.bashrc
-    colors = "always"; # Всегда использовать цвета
-    icons = "always"; # Всегда показывать иконки
-  };
-}
--- nixos-config/overlay_stable.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  environment.systemPackages = with pkgs.stable; [
-    # gcc
-    #    wezterm
-    # zathura
-    duckstation
-  ];
-}
